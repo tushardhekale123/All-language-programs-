ce61f260f0efad5dd915e3ac87ce681f1604dd72
tushardhekale123 <tushardhekale884@gmail.com>
2026-02-02 17:32:23 +0530

    Added casting.java program

---
+++ java/casting.java
@@ -0,0 +1,11 @@
+public class casting {
+    public static void main (String[] args) {
+       int a = 65;
+       byte b = (byte)a;
+         System.out.println(b);
+
+         float f = 12.5f;
+         int t = (int)f;
+           System.out.println(t);
+    }
+}
