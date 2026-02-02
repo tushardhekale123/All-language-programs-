0138db03cd2fb9490b30a539fbbc2477266e2a4e
tushardhekale123 <tushardhekale884@gmail.com>
2026-02-02 16:10:42 +0530

    Added variable.java program

--- java/variable.java
+++ java/variable.java
@@ -2,7 +2,8 @@ public class variable {
     public static void main(String[] args){
         int num1 = 10;
         int num2 = 20;
-        int result = num1 + num2;
-        System.out.println(result);
+        String name = "John";
+        int result = num1 + num2 ;
+        System.out.println(result + "\n" + name);
     }
 }
