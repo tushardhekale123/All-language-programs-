6208d310cdc5c2b1b5aaf3989d2fc72daaf73f7a
tushardhekale123 <tushardhekale884@gmail.com>
2026-02-03 18:20:01 +0530

    Added relational.java program

---
+++ java/relational.java
@@ -0,0 +1,33 @@
+public class relational {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        boolean isEqual = (num1 == num2);
+        boolean isNotEqual = (num1 != num2);
+        boolean isGreater = (num1 > num2);
+        boolean isLess = (num1 < num2);
+        boolean isGreaterOrEqual = (num1 >= num2);
+        boolean isLessOrEqual = (num1 <= num2);
+
+        System.out.println("Is Equal: " + isEqual);
+        System.out.println("Is Not Equal: " + isNotEqual);
+        System.out.println("Is Greater: " + isGreater);
+        System.out.println("Is Less: " + isLess);
+        System.out.println("Is Greater Or Equal: " + isGreaterOrEqual);
+        System.out.println("Is Less Or Equal: " + isLessOrEqual);
+    }
+}
+
+
+// public class relational {
+//     public static void main(String[] args) {
+//         int x = 10;
+//         int y = 5;
+
+//     boolean result = x < y;
+
+//     System.out.println("Result of x < y: " + result);
+
+//     }
+// }
