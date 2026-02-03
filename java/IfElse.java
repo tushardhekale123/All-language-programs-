63e1924ed74b2e625cb7edf24a6141887d190d4b
tushardhekale123 <tushardhekale884@gmail.com>
2026-02-03 19:17:52 +0530

    Added IfElse.java program

---
+++ java/IfElse.java
@@ -0,0 +1,59 @@
+// public class IfElse {
+//     public static void main(String[] args) {
+//         int x =20 ;
+
+//         if (x>10 && x<=19) 
+//         System.err.println("HELLO");
+//     }
+// }
+
+
+// public class IfElse {
+//     public static void main(String[] args) {
+//         int x =20 ;
+
+//         if (x>10) 
+//         System.err.println("HELLO");
+
+//         else
+//         System.out.println("BYE");
+//     }
+// }
+
+
+// public class IfElse {
+//     public static void main(String[] args) {
+//         int x =20 ;
+//         int y =15 ;
+//         int z =5 ;
+
+//         if (x>10 && x<=19) 
+//         System.err.println(x);
+
+//         else if (y<z && y>x)
+//         System.out.println(y);
+
+//         else 
+//         System.out.println(z);
+//     }
+// }
+
+
+
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x =20 ;
+        int y =15 ;
+        int z =5 ;
+
+        if (x>10 && x<=19) 
+        System.err.println(x);
+
+        else if (y>z && y<x)
+        System.out.println(y);
+
+        else 
+        System.out.println(z);
+    }
+}
