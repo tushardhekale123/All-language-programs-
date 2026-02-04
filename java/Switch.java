cbb04ade50235d8a8cb3e801d628499a9faa7e07
tushardhekale123 <tushardhekale884@gmail.com>
2026-02-04 18:26:04 +0530

    Added all.java program

---
+++ java/Switch.java
@@ -0,0 +1,28 @@
+public class Switch {
+    public static void main(String[] args) {
+        int n = 3;
+
+            switch (18) {
+
+                    case 1:
+                    System.out.println("calling");
+                    break;
+
+                    case 2:
+                    System.out.println("massage");
+                    break;      
+
+                    case 3:
+                    System.out.println("internet");
+                    break;
+
+                    case 4:
+                    System.out.println("settings");
+                    break;              
+
+                    default:
+                    System.out.println("invalid");
+
+            }
+        }
+    }
---
+++ java/Ternary.java
@@ -0,0 +1,25 @@
+
+
+// public class Ternary {
+//     public static void main(String[] args) {
+//         int x = 10 ;
+//         int y = 5 ;
+
+//         int Result = x > y ? x : y;
+//         System.out.println(Result);
+//     }
+// }
+
+
+
+
+
+public class Ternary {
+    public static void main(String[] args) {
+        int x = 10 ;
+        int y = 5 ;
+
+        int Result = (x < y) ? x : y ;
+        System.out.println(Result);
+    }
+}
