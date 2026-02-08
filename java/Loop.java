a1c77486cc70b5d5e18537e88e2b674486810ec3
tushardhekale123 <tushardhekale884@gmail.com>
2026-02-09 00:37:01 +0530

    Loop file.java

--- java/Loop.java
+++ java/Loop.java
@@ -1,7 +1,4 @@
 
-
-// public class Loop {
-//     public static void main(String[] args) {
 //         for (int i= 0; i<=4; i++)
 //          System.out.println("Tushar");
 //     }
@@ -36,19 +33,94 @@
 // }
 
 
-public class Loop {
-    public static void main(String[] args) {
+// public class Loop {
+//     public static void main(String[] args) {
         
-        int i= 1, j=1;
+//         int i= 1, j=1;
 
-        while (i<=5) {
-        System.out.println("Tushar");
-            while (j<=10) {
-            System.out.println("munna");
-            j++;
-            }
-            i++;
+//         while (i<=5) {
+//         System.out.println("Tushar");
+//             while (j<=10) {
+//             System.out.println("munna");
+//             j++;
+//             }
+//             i++;
+//         }
+//     }
+// }
+
+
+
+// public class Loop {
+//     public static void main(String[] args) {
+        
+//         for (int i=1;  i<=5; i++) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+// public class Loop {
+//     public static void main(String[] args) {
+//     int i = 2;
+//     while (i <= 10) {
+//     System.out.println(i);
+//     i += 2;
+//       }
+//     }
+//  }
+
+
+// public class Loop {
+//     public static void main(String[] args) {
+//     int i = 1;
+//     do  {
+//         System.err.println("Tushar");
+//         i++;
+//       }  while (i<=5);
+//     }
+//  }  
+
+
+
+// public class Loop {
+//     public static void main(String[] args) {
+//         for (int i=10; i>=1 ; i--) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// public class Loop {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int sum = 0;
+//         for (int i=1; i<=n; i++) {
+//            sum += i;                                                            
+//         }System.out.println(sum);
+//     }
+// }
+
+
+// public class Loop {
+//     public static void  main (String[] args) {
+//         int sum = 0;
+//         for ( int i=1;i<=10;i++)
+//          {
+//             sum += i;
+//         } System.out.println(sum);
+//     }   
+// }
+
+
+class Loop {
+    public static void  main (String[] args) {
+        int no = 5;
+        for (int i=1;i<=10;i++){
+            System.out.println(i*no);
         }
     }
 }
-
