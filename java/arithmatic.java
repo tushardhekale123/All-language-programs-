e71c96cb689176d7527f1d8cdf0a3aa7fdf1f439
tushardhekale123 <tushardhekale884@gmail.com>
2026-06-20 17:35:28 +0530

    add file

--- java/arithmatic.java
+++ java/arithmatic.java
@@ -1,18 +1,64 @@
+
+import java.util.Scanner;
+
+
+// import java.util.Scanner;
+
+// public class arithmatic {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out .println ("Enter first number");
+//         int num1 = sc.nextInt ();
+
+//         System.out .println ("Enter second number");
+//         int num2 = sc.nextInt ();
+
+//         int sum = num1 + num2;
+//         int difference = num1 - num2;
+//         int product = num1 * num2;
+//         int quotient = num1 / num2;
+//         int remainder = num1 % num2;
+//         System.out.println("Sum: " + sum);
+//         System.out.println("Difference: " + difference);
+//         System.out.println("Product: " + product);
+//         System.out.println("Quotient: " + quotient);
+//         System.out.println("Remainder: " + remainder);
+//     }
+// }
+
+
+
+
+// import java.util.Scanner;
+
+// public class arithmatic {
+
+// public static void main (String args[]) {
+
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("Enter first no");
+//     int num1 = sc.nextInt();
+//     System.out.println("Enter second no");       
+//     int num2 = sc.nextInt();    
+    
+//     int sum  = num1 + num2;
+//     System.out.println("Sum: " + sum);
+//   }
+// }
 public class arithmatic {
-    public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 5;
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
 
-        int sum = num1 + num2;
-        int difference = num1 - num2;
-        int product = num1 * num2;
-        int quotient = num1 / num2;
-        int remainder = num1 % num2;
+        System.out.println("Enter first number");
+        int n = sc.nextInt();
 
-        System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + difference);
-        System.out.println("Product: " + product);
-        System.out.println("Quotient: " + quotient);
-        System.out.println("Remainder: " + remainder);
+        System.out.println ("Enter second number");
+        int m = sc.nextInt();
+
+        int sum = n+m;
+        System.out.println(n+m);
     }
 }
