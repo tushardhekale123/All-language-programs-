be2029004eadaf27ee760957db989e703a0f8c3b
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-04 17:03:25 +0530

    practice

--- java practice/control.java
+++ java practice/control.java
@@ -1,5 +1,77 @@
-package java practice;
+// class control {
+//     public static void main (String [] args){
+//         System.out.println("Hello World");
+//     }
+// }
 
-public class control {
-    
-}
+
+
+// class control {
+//     public static void main (String [] args){
+//         int n = 10;
+//         double percentage = 75.5;
+//         char grade = 'A';
+//         boolean isPassed = true;
+//         String name = "Tushar";
+
+//         System.out.println("n = " + n);
+//         System.out.println("percentage = " + percentage);
+//         System.out.println("grade = " + grade);
+//         System.out.println("isPassed = " + isPassed);
+//         System.out.println("name = " + name);
+
+//     }
+// }
+
+
+
+// class control {
+//     public static void main (String [] args){
+//         int n = 10;
+//         double m = 75.5;
+//         double sum = n + m;
+
+//         System.out.println("Sum = " + sum);
+//     }
+// }
+
+// import java.util.Scanner;
+// class control {
+//     public static void main (String [] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter your name");
+//         String name = sc.nextLine();
+
+//         System.out.println(name);
+//     }
+// }
+
+
+
+
+//     import java.util.Scanner;
+//     class control {
+//     public static void main (String [] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         while (true) {
+//         System.out.println("Enter length = ");
+//         double length = sc.nextDouble();
+
+//         System.out.println("Enter breadth = ");
+//         double breadth = sc.nextDouble();
+
+//         double area = length * breadth;
+//         System.out.println("Area of rectangle = " + area);
+
+//         System.out.println("yes/no");
+//         String ch = sc.next();
+
+//         if (ch.equals("no"))
+//         break;
+//         }
+//     }
+// }
+
+
