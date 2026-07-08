4cbcec625caac7ac7ae70603b227917b13818f5e
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-08 23:08:52 +0530

    reverse

--- java practice/control.java
+++ java practice/control.java
@@ -294,16 +294,38 @@
 //     }
 // }
 
+
+
+
+// import java.util.Scanner;
+// class control {
+//     public static void main (String [] args) {
+//         Scanner sc = new Scanner(System.in);
+       
+//         System.out.println("Enter no = ");
+//         int n = sc.nextInt();
+
+//         for (int i = 1; i <= n; i++ ){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 class control {
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
-       
+
         System.out.println("Enter no = ");
         int n = sc.nextInt();
 
-        for (int i = 1; i <= n; i++ ){
-            System.out.println(i);
+        int reverse = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
         }
+        System.out.println("Reverse = " + reverse);
     }
 }
