46068c189d1e3d2d20ab9d289d0289bfdfc9b262
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-21 23:39:36 +0530

    new fail

---
+++ java practice/arrys.java
@@ -0,0 +1,31 @@
+
+// public class arrya {
+//     public static void main(String[]args) {
+//         int marks [] = new int[5]; 
+//         marks[0] = 98;
+//         marks[1] = 68;
+//         marks[2] = 34;
+//         marks[3] = 76;
+
+//         System.out.println(marks[0]);
+//         System.out.println(marks[1]);
+//         System.out.println(marks[2]);
+//         System.out.println(marks[3]);
+//         System.out.println(marks[4]);
+//     }    
+// }
+
+
+// class arrays {
+//     public static void main(String[] args) {
+//         int marks [] = new int [4];
+//         marks[0] = 56;
+//         marks[1] = 89;
+//         marks[2] = 23;
+//         marks[3] = 56;
+
+//         for (int i=0; i<=4; i++){
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
