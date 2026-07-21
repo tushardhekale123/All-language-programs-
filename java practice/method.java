7f9a134ae79c613e3d806eb6c4181185c2f90707
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-21 23:36:14 +0530

    new fail

--- java practice/method.java
+++ java practice/method.java
@@ -281,16 +281,35 @@
 
 // Factorial using recursion.
 
+// class method {
+//     static int fact(int n) {
+//         if (n == 0) {
+//         return 1;
+//     }
+//     return n * fact (n - 1 );
+// }
+//         public static void main (String[]args) {
+//             System.out.println(fact(5));
+//         }
+//     }
+
+
+
+// Fibonacci using recursion.
+
 class method {
-    static int fact(int n) {
-        if (n == 0) {
-            return 1;
-        }
 
-        return n * fact(n - 1);
-        }
-        public static void main(String[]args) {
-            System.out.println(fact(5));
+    static int fib(int n) {
 
-        }
+        if (n == 0 || n == 1)
+            return n;
+
+        return fib(n - 1) + fib(n - 2);
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(fib(5));
+
+    }
+}
