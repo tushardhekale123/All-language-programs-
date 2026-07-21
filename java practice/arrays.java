4b0c584c67996f7bfe44d08d6b99e37bfac53d03
tushardhekale123 <tushardhekale884@gmail.com>
2026-07-22 00:20:10 +0530

    new fail

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+
+
 // public class arrya {
 //     public static void main(String[]args) {
 //         int marks [] = new int[5]; 
@@ -29,3 +32,24 @@
 //         }
 //     }
 // }
+
+
+class arrays{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Enter array length = ");
+        int n = sc.nextInt();
+        int number[] = new int[n];
+
+        System.out.println("Enter marks = ");
+        for (int i=0; i<n; i++){
+        number[i] = sc.nextInt();
+       }
+       System.out.println("Arraya element ");
+       for (int i=0; i<n; i++) {
+        System.out.println(number[i]);
+       }
+       sc.close();
+    }
+}
