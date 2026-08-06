75b7f334ae55e5d07801db414eb5d4d665fb3057
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-06 22:20:32 +0530

    Print 2D Array

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -702,6 +702,7 @@
 // Row-wise Sum
 // Column-wise Sum
 // Average of Matrix Elements
+
 // Intermediate Level
 // Main Diagonal Elements
 // Secondary Diagonal Elements
@@ -713,6 +714,7 @@
 // Matrix Multiplication
 // Identity Matrix
 // Symmetric Matrix
+
 // Advanced Level
 // Upper Triangle Matrix
 // Lower Triangle Matrix
@@ -725,3 +727,17 @@
 // Saddle Point
 // Search in Sorted Matrix
 
+
+// Print 2D Array (Matrix)
+
+class arrays {
+    public static void main(String[] args) {
+        int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 },{ 70, 80, 90 } };
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+                 }
+                 System.out.println();
+         }
+    }
+}
