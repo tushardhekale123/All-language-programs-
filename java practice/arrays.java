3ffcde419a5023cdb593ae5e7b078ff93975d2ee
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-06 22:41:40 +0530

    Print 2D Array with Scanner

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -730,14 +730,43 @@
 
 // Print 2D Array (Matrix)
 
+// class arrays {
+//     public static void main(String[] args) {
+//         int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 },{ 70, 80, 90 } };
+//             for (int i = 0; i < arr.length; i++) {
+//                 for (int j = 0; j < arr.length; j++) {
+//                 System.out.print(arr[i][j] + " ");
+//                  }
+//                  System.out.println();
+//          }
+//     }
+// }
+
+import java.util.Scanner;
 class arrays {
     public static void main(String[] args) {
-        int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 },{ 70, 80, 90 } };
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
-                 }
-                 System.out.println();
-         }
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Row Size");
+        int row = sc.nextInt();
+
+        System.out.println("Enter Column Size");
+        int col = sc.nextInt();
+
+        int arr[][] = new int [row][col];
+
+        System.out.println("Enter Element");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix Element");
+        for (int i = 0; i  < row; i++) {
+                for (int j = 0; j < col; j++)  {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
     }
-}
