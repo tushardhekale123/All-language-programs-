042c51145f4d09b9028a0d54ca2f2c18f3c482ba
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-06 23:10:47 +0530

    Search an Element in Matrix

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -742,31 +742,58 @@
 //     }
 // }
 
-import java.util.Scanner;
+
+
+// import java.util.Scanner;
+// class arrays {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter Row Size");
+//         int row = sc.nextInt();
+
+//         System.out.println("Enter Column Size");
+//         int col = sc.nextInt();
+
+//         int arr[][] = new int [row][col];
+
+//         System.out.println("Enter Element");
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         System.out.println("Matrix Element");
+//         for (int i = 0; i  < row; i++) {
+//                 for (int j = 0; j < col; j++)  {
+//                     System.out.print(arr[i][j] + " ");
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//     }
+
+
+//Search an Element in Matrix
+
 class arrays {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main (String [] args) {
+        int arr[][] = { { 10,20,30 }, { 40,50,60 },{ 70,80,90 } };
 
-        System.out.println("Enter Row Size");
-        int row = sc.nextInt();
+        int x = 50;
 
-        System.out.println("Enter Column Size");
-        int col = sc.nextInt();
-
-        int arr[][] = new int [row][col];
-
-        System.out.println("Enter Element");
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                arr[i][j] = sc.nextInt();
+        for ( int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
             }
+            System.out.println();
         }
-        System.out.println("Matrix Element");
-        for (int i = 0; i  < row; i++) {
-                for (int j = 0; j < col; j++)  {
-                    System.out.print(arr[i][j] + " ");
+        for ( int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                if (arr[i][j] == x) {
+                    System.out.println("Element Found at Index = " + (i+1) + " " + (j+1));
                 }
-                System.out.println();
             }
         }
-    }
+    } 
+}
