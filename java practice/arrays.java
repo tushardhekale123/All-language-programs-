63166e099db5778b532ead2e167925564a4912df
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-08 23:51:45 +0530

    code repeted

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -835,3 +835,25 @@
 //         }
 //     }
 // }
+
+class arrays {
+    public static void main (String [] args) {
+        int arr[][] = { { 10,20,30 }, { 40,50,60 },{ 70,80,90 } };
+
+        int x = 50;
+
+        for ( int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for ( int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                if (arr[i][j] == x) {
+                    System.out.println("Element Found at Index = " + (i+1) + " " + (j+1));
+                }
+            }
+        }
+    } 
+}
