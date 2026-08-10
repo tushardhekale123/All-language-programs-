15bed0d425c26a2b74993c01ed44f596a84308be
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-10 22:17:27 +0530

    Main Diagonal Elements

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -854,30 +854,56 @@
 // }
 
 
+// import java.util.Scanner;
+// class arrays {
+//     public static void main (String[] args) {
+//         Scanner sc = new Scanner (System.in);
+
+//         System.out.println("Enter Row Size");
+//         int row = sc.nextInt();
+
+//         System.out.println("Enter Column Size");
+//         int col = sc.nextInt();
+
+//         int arr[][] = new int [row][col];
+
+//         for (int i=0; i<row; i++) {
+//             for (int j=0; j<col; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         int sum = 0;
+//         for (int i=0; i<row; i++) {
+//             for (int j=0; j<col; j++) {
+//                 sum = sum + arr[i][j];
+//             }
+//         }
+//         System.out.println("Sum of All Elements: " + sum);
+//         }
+//     }
+
+
+
+// Main Diagonal Elements
+
+// class arrays {
+//     public static void main(String[] args) {
+//         int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
+
+//         System.out.println("Main Diagonal Elements: ");
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr.length; j++) {
+//                 if (i == j) {
+//                     System.out.print(arr[i][j] + " ");
+//                 }
+//             }
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 class arrays {
     public static void main (String[] args) {
-        Scanner sc = new Scanner (System.in);
-
-        System.out.println("Enter Row Size");
-        int row = sc.nextInt();
-
-        System.out.println("Enter Column Size");
-        int col = sc.nextInt();
-
-        int arr[][] = new int [row][col];
-
-        for (int i=0; i<row; i++) {
-            for (int j=0; j<col; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        int sum = 0;
-        for (int i=0; i<row; i++) {
-            for (int j=0; j<col; j++) {
-                sum = sum + arr[i][j];
-            }
-        }
-        System.out.println("Sum of All Elements: " + sum);
-        }
+        
     }
+}
