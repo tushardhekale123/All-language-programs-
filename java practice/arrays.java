cbbe8a730016c73f0487229b8233ec380071c9ff
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-10 21:26:46 +0530

    sum of all arrays with Scanner

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -839,16 +839,45 @@
 
 // Sum of All Elements
 
+// class arrays {
+//     public static void main (String[] args) {
+//         int arr [][] = { { 10,20,30 }, { 40,50,60 },{ 70,80,90 } };
+
+//         int sum = 0;
+//         for  (int i=0; i<arr.length; i++) {
+//             for (int j=0; j<arr.length; j++) {
+//                 sum = sum + arr[i][j];
+//             }
+//         }
+//         System.out.println("Sum of All Elements: " + sum);
+//     }
+// }
+
+
+import java.util.Scanner;
 class arrays {
     public static void main (String[] args) {
-        int arr [][] = { { 10,20,30 }, { 40,50,60 },{ 70,80,90 } };
+        Scanner sc = new Scanner (System.in);
 
+        System.out.println("Enter Row Size");
+        int row = sc.nextInt();
+
+        System.out.println("Enter Column Size");
+        int col = sc.nextInt();
+
+        int arr[][] = new int [row][col];
+
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
         int sum = 0;
-        for  (int i=0; i<arr.length; i++) {
-            for (int j=0; j<arr.length; j++) {
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
                 sum = sum + arr[i][j];
             }
         }
         System.out.println("Sum of All Elements: " + sum);
+        }
     }
-}
