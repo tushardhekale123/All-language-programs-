fe55ff7e8993144b69f1f822af4e836b5b96967e
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-10 23:43:53 +0530

    Second Diagonal Elements sum

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -951,6 +951,37 @@
 //     }
 
 
+// import java.util.Scanner;
+// class arrays {
+//     public static void main (String []args) {
+//         Scanner sc = new Scanner (System.in);
+//          System.out.println("Enter Row Size");
+//          int row = sc.nextInt();    
+
+//          System.out.println("Enter Column Size");
+//          int col = sc.nextInt();
+
+//          int arr[][] = new int [row][col];
+
+//          System.out.println("Enter Element");
+//          for (int i=0; i<row; i++) {
+//              for (int j=0; j<col; j++) {
+//                  arr[i][j] = sc.nextInt();
+//              }
+//          }
+//          for (int i=0; i<row; i++) {
+//              for (int j=0; j<col; j++) {
+//                  if (i + j == arr.length -1) {
+//                      System.out.print(arr[i][j] + " ");
+//                  }
+//              }
+//          }
+//     }
+// }
+
+
+// Secondary Diagonal Elements Sum
+
 import java.util.Scanner;
 class arrays {
     public static void main (String []args) {
@@ -975,6 +1006,15 @@ class arrays {
                      System.out.print(arr[i][j] + " ");
                  }
              }
+         }
+         int sum = 0;
+         for (int i=0; i<row; i++) {
+             for (int j=0; j<col; j++) {
+                 if (i + j == arr.length -1) {
+                     sum = sum + arr[i][j]; 
+                 }
+             }
+             System.out.println("Sum of Secondary Diagonal Elements: " + sum);
          }
     }
 }
