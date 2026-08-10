405b4664e3537c3e5157f8b45e4b45ef9338c22c
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-10 23:21:19 +0530

    Main Diagonal Elements Scanne

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -901,31 +901,51 @@
 //     }
 // }
 
-import java.util.Scanner;
+
+
+// import java.util.Scanner;
+// class arrays {
+//     public static void main (String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter Row Size");
+//         int row = sc.nextInt();     
+
+//         System.out.println("Enter Column Size");
+//         int col = sc.nextInt();
+
+//         int arr[][] =new int [row][col];
+
+//         System.out.println("Enter Element");
+//         for (int i=0; i<row; i++) {
+//             for (int j=0; j<col; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         for (int i=0; i<row; i++) {
+//             for (int j=0; j<col; j++) {
+//                 if (i == j) {
+//                     System.out.print(arr[i][j] + " ");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// Secondary Diagonal Elements
+ 
 class arrays {
     public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
 
-        System.out.println("Enter Row Size");
-        int row = sc.nextInt();     
-
-        System.out.println("Enter Column Size");
-        int col = sc.nextInt();
-
-        int arr[][] =new int [row][col];
-
-        System.out.println("Enter Element");
-        for (int i=0; i<row; i++) {
-            for (int j=0; j<col; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        for (int i=0; i<row; i++) {
-            for (int j=0; j<col; j++) {
-                if (i == j) {
+        System.out.println("Secondary Diagonal Elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i + j == arr.length -1) {
                     System.out.print(arr[i][j] + " ");
+                }
                 }
             }
         }
     }
-}
