6d919e67bd1d0a04998b92ad4131aa8428f642aa
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-10 22:21:29 +0530

    Main Diagonal Elements Scanner

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -904,6 +904,28 @@
 import java.util.Scanner;
 class arrays {
     public static void main (String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Row Size");
+        int row = sc.nextInt();     
+
+        System.out.println("Enter Column Size");
+        int col = sc.nextInt();
+
+        int arr[][] =new int [row][col];
+
+        System.out.println("Enter Element");
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (i == j) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+        }
     }
 }
