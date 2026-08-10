9cd67c9d869a3a60df1b7b8e51b5887f35b642ae
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-10 21:14:52 +0530

    sum of all arrays

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -836,3 +836,19 @@
 //     }
 // }
 
+
+// Sum of All Elements
+
+class arrays {
+    public static void main (String[] args) {
+        int arr [][] = { { 10,20,30 }, { 40,50,60 },{ 70,80,90 } };
+
+        int sum = 0;
+        for  (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                sum = sum + arr[i][j];
+            }
+        }
+        System.out.println("Sum of All Elements: " + sum);
+    }
+}
