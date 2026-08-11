cf41f12a64dfe760a1c4343628b7fabb1980eaff
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-11 18:33:27 +0530

    evean element

--- java practice/traversal.java
+++ java practice/traversal.java
@@ -55,11 +55,25 @@
 
 // array elements in reverse order.
 
+// class traversal {
+//     public static void main (String [] args) {
+//         int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//         for (int i = arr.length-1; i>=0; i--) {
+//             System.out.print(arr[i]+ " ");
+//         }
+//     }
+// }
+
+// Print elements at even indexes.
+
 class traversal {
+    
     public static void main (String [] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9,10};
-        for (int i = arr.length-1; i>=0; i--) {
-            System.out.print(arr[i]+ " ");
+        for (int i = 0; i<arr.length; i++) {
+            if (i%2==0) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
