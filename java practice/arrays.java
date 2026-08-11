c5ca36c618ee5cb3f5f6645e70469020e3af9d59
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-11 17:21:16 +0530

    Lower Triangle Matrix Scanner

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -1040,26 +1040,63 @@
 //     }
 // }
 
+
+
+// import java.util.Scanner;
+// class arrays {
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("Enter Row Size");
+//     int row = sc.nextInt();
+
+//     System.out.println("Enter Column Size");
+//     int col = sc.nextInt();
+
+//     int arr[][] = new int[row][col];
+
+//     System.out.println("Enter Element");
+//     for (int i = 0; i < row; i++) {
+//         for (int j = 0; j < col; j++) {
+//             arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         System.out.println("Upper Triangle Matrix: ");
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < col; j++) {
+//                 if (j >= i) {
+//                     System.out.print(arr[i][j] + " ");
+//                 } else {
+//                     System.out.print("0 ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// Lower Triangle Matrix
+
 import java.util.Scanner;
 class arrays {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter Row Size");
-    int row = sc.nextInt();
+        System.out.println("Enter Row Size");
+        int row = sc.nextInt();
 
-    System.out.println("Enter Column Size");
-    int col = sc.nextInt();
+        System.out.println("Enter Column Size");
+        int col = sc.nextInt();
 
-    int arr[][] = new int[row][col];
+        int arr[][] = new int[row][col];
 
-    System.out.println("Enter Element");
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            arr[i][j] = sc.nextInt();
+        System.out.println("Enter Element");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Upper Triangle Matrix: ");
+        System.out.println("Lower Triangle Matrix: ");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (j >= i) {
