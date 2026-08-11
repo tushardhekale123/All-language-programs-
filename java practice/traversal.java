0d40d6821f8b1defc964de5a68a698c57a4d43ac
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-11 18:50:09 +0530

    first and last

--- java practice/traversal.java
+++ java practice/traversal.java
@@ -66,14 +66,32 @@
 
 // Print elements at even indexes.
 
-class traversal {
+// class traversal {
     
-    public static void main (String [] args) {
-        int arr[] = {1,2,3,4,5,6,7,8,9,10};
-        for (int i = 0; i<arr.length; i++) {
-            if (i%2==0) {
-                System.out.print(i + " ");
-            }
-        }
-    }
-}
+//     public static void main (String [] args) {
+//         int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//         for (int i = 0; i<arr.length; i++) {
+//             if (i%2==0) {
+//                 System.out.print(i + " ");
+//             }
+//         }
+//     }
+// }
+
+
+// Print array elements with their indexes.
+
+// class traversal {
+//     public static void main (String [] args) {
+//         int arr[] = {10,20,30,40,50,60,70,80,90,100};
+//             System.out.println( arr[0]);
+//     }
+// }
+
+
+// class traversal {
+//     public static void main (String [] args) {
+//         int arr[] = {10,20,30,40,50,60,70,80,90,100};
+//             System.out.println(arr[arr.length-1]);
+//     }
+// }
