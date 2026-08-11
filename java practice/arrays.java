91444f213c70daa813ac9420f77a7a178fffcc81
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-11 17:18:00 +0530

    Upper Triangle Matrix Scanner

--- java practice/arrays.java
+++ java practice/arrays.java
@@ -1022,14 +1022,47 @@
 
 // Upper Triangle Matrix
 
+// class arrays {
+//     public static void main(String[] args) {
+//         int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
+
+//         System.out.println("Upper Triangle Matrix: ");
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j=0; j<arr.length; j++) {
+//                 if (j >= i ) {
+//                     System.out.print(arr[i][j] + " ");
+//                 } else {
+//                     System.out.print("0 ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+import java.util.Scanner;
 class arrays {
     public static void main(String[] args) {
-        int arr[][] = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 } };
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("Enter Row Size");
+    int row = sc.nextInt();
+
+    System.out.println("Enter Column Size");
+    int col = sc.nextInt();
+
+    int arr[][] = new int[row][col];
+
+    System.out.println("Enter Element");
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            arr[i][j] = sc.nextInt();
+            }
+        }
         System.out.println("Upper Triangle Matrix: ");
-        for (int i = 0; i < arr.length; i++) {
-            for (int j=0; j<arr.length; j++) {
-                if (j >= i ) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (j >= i) {
                     System.out.print(arr[i][j] + " ");
                 } else {
                     System.out.print("0 ");
