406d8b94b9d376a22b57fa6001c4afa657b8a6dc
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-11 19:02:45 +0530

    count even and odd

--- java practice/traversal.java
+++ java practice/traversal.java
@@ -95,3 +95,24 @@
 //             System.out.println(arr[arr.length-1]);
 //     }
 // }
+
+
+// Count even and odd numbers.
+
+class traversal {
+    public static void main (String [] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int even = 0;
+        int odd = 0;
+
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] % 2 == 0){
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
+}
