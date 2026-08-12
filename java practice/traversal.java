fb332d777ea6597b5986ee84c7e7a0d0b30d93e6
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-12 22:43:33 +0530

    new

--- java practice/traversal.java
+++ java practice/traversal.java
@@ -196,34 +196,39 @@
 // }
 
 
-import java.util.Scanner;
-class traversal {
-    public static void main (String []args) {
-        Scanner sc = new Scanner (System.in);
+// import java.util.Scanner;
+// class traversal {
+//     public static void main (String []args) {
+//         Scanner sc = new Scanner (System.in);
 
-        System.out.print("Enter the size of the array: ");
-        int n = sc.nextInt();
+//         System.out.print("Enter the size of the array: ");
+//         int n = sc.nextInt();
 
-        int arr[] = new int[n];
+//         int arr[] = new int[n];
 
-        System.out.println("Enter the elements of the array: ");
-        for (int i=0; i<n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        boolean visited[] = new boolean[n];
+//         System.out.println("Enter the elements of the array: ");
+//         for (int i=0; i<n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         boolean visited[] = new boolean[n];
 
-        for (int i=0; i<n; i++) {
-            if (visited[i] == true) {
-                continue;
-            }
-            int count = 1;
-            for (int j=i+1; j<n; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                    visited[j] = true;
-                }
-            }
-            System.out.println(arr[i] + " = " + count);
-        }
-    }
-}
+//         for (int i=0; i<n; i++) {
+//             if (visited[i] == true) {
+//                 continue;
+//             }
+//             int count = 1;
+//             for (int j=i+1; j<n; j++) {
+//                 if (arr[i] == arr[j]) {
+//                     count++;
+//                     visited[j] = true;
+//                 }
+//             }
+//             System.out.println(arr[i] + " = " + count);
+//         }
+//     }
+// }
+
+
+
+
+// Move all zeroes to the end.
