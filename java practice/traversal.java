4187e7e8699565aa4cc08f104db8ecb57057a379
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-12 22:57:59 +0530

    all zero end

--- java practice/traversal.java
+++ java practice/traversal.java
@@ -232,3 +232,24 @@
 
 
 // Move all zeroes to the end.
+
+class traversal {
+    public static void main(String[] args) {
+        int arr[] = {0,5,0,5,3,4,0,1};
+
+        int index = 0;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
