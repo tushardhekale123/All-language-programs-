ed108a82a541967575fd2e338c6531c7e42ae736
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-12 21:58:03 +0530

    frequency

--- java practice/traversal.java
+++ java practice/traversal.java
@@ -152,20 +152,45 @@
 
 
 
-class traversal {
-    public static void main(String[] args) {
-        int arr[] = { 100, 20, 30, 40, 50, 60, 70, 80, 90, 10 };
-        int largest = arr[0];
-        int secondLargest = arr[1];
+// class traversal {
+//     public static void main(String[] args) {
+//         int arr[] = { 100, 20, 30, 40, 50, 60, 70, 80, 90, 10 };
+//         int largest = arr[0];
+//         int secondLargest = arr[1];
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < largest) {
-                secondLargest = largest;
-                largest = arr[i];
-            } else if (arr[i] < secondLargest && arr[i] != largest) {
-                secondLargest = arr[i];
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] < largest) {
+//                 secondLargest = largest;
+//                 largest = arr[i];
+//             } else if (arr[i] < secondLargest && arr[i] != largest) {
+//                 secondLargest = arr[i];
+//             }
+//         }
+//         System.out.println("Second Smallest Element: " + secondLargest);
+//     }
+// }
+
+
+// Find frequency of every element.
+
+class traversal {
+    public static void main (String [] args) {
+        int arr[] = {1,2,3,4,5,2,3,2,1,2,5,};
+
+        boolean visited[] = new boolean[arr.length];
+
+        for (int i=0; i<arr.length; i++) {
+            if (visited[i] == true) {
+                continue;
             }
+            int count = 1;
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                 }
+            }
+            System.out.println("Element " + arr[i] + " occurs " + count + " times");
         }
-        System.out.println("Second Largest Element: " + secondLargest);
     }
 }
