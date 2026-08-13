6b1057ba624ae7d7f7be7098ea032dda1cec9104
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-13 12:14:21 +0530

    length With Scanner

--- java practice/string.java
+++ java practice/string.java
@@ -7,3 +7,14 @@
 //     }
 // }
 
+import java.util.Scanner;
+public class string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+         
+        System.out.println("Enter name");
+        String n = sc.nextLine();
+
+        System.out.println("Length = " + n.length());
+    }
+}
