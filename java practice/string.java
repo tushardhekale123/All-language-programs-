abbf80a4f7ecc6c9ba1fe639faf05bf6e6f11660
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-13 12:06:15 +0530

    length

---
+++ java practice/string.java
@@ -0,0 +1,9 @@
+//  Name Length
+
+// public class string {
+//     public static void main(String[] args) {
+//         String name = "Tushar";
+//         System.out.println(name.length());
+//     }
+// }
+
