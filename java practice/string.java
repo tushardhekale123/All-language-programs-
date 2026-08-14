16994fb022806e967b68b78e469432393c6637fc
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-14 23:15:25 +0530

    complete

--- java practice/string.java
+++ java practice/string.java
@@ -270,10 +270,14 @@
 
 // Delete
 
-class string {
-    public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer("Hello Java");
-        sb.delete(0,3);
-        System.out.println(sb);
-    }
-}
+// class string {
+//     public static void main(String[] args) {
+//         StringBuffer sb = new StringBuffer("Hello Java");
+//         sb.delete(0,3);
+//         System.out.println(sb);
+//     }
+// }
+
+
+
+//  Complete String 
