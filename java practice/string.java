bd89f02ba8e67920b40df32b05f883b8f78123e6
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-14 23:08:21 +0530

    insert

--- java practice/string.java
+++ java practice/string.java
@@ -228,10 +228,20 @@
 
 // deleteCharAt
 
+// class string {
+//     public static void main(String[] args) {
+//         StringBuffer sb = new StringBuffer("JVAA");
+//         sb.deleteCharAt(1);
+//         System.out.println(sb);
+//     }
+// }
+
+// insert()
+
 class string {
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer("JVAA");
-        sb.deleteCharAt(1);
+        StringBuffer sb = new StringBuffer("Java");
+        sb.insert(0,"DSA ");
         System.out.println(sb);
     }
 }
---
+++ java practice/tempCodeRunnerFile.java
@@ -0,0 +1,32 @@
+//         System.out.println(sb);
+//     }
+// }
+
+// length
+
+// class string {
+//     public static void main(String[] args) {
+//         StringBuffer sb = new StringBuffer("JAVA");
+//         System.out.println(sb.length());
+//     }
+// }
+
+// deleteCharAt
+
+// class string {
+//     public static void main(String[] args) {
+//         StringBuffer sb = new StringBuffer("JVAA");
+//         sb.deleteCharAt(1);
+//         System.out.println(sb);
+//     }
+// }
+
+// insert()
+
+class string {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Java");
+        sb.insert(0,"DSA ");
+        System.out.println(sb);
+    }
+}
