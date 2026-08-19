55c42cf94229511692be054c06860df5e561a68a
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-19 23:48:05 +0530

    class rectagle

--- java practice/oop.java
+++ java practice/oop.java
@@ -25,33 +25,58 @@
 //     }
 // }
 
-class Car {
-    String brand;
-    String color;
 
-    void drive() {
-        System.out.println(brand +"is driving");
+
+// Car
+
+// class Car {
+//     String brand;
+//     String color;
+
+//     void drive() {
+//         System.out.println(brand +"is driving");
+//     }
+// }
+// public class oop {
+//     public static void main(String[] args) {
+//         Car c1 = new Car();
+
+//         c1.brand = "BMW";
+//         c1.color = "Black";
+
+//         Car c2 = new Car(); 
+
+//         c2.brand = "Audi";
+//         c2.color = "White";
+
+//         System.out.println(c1.brand);
+//         System.out.println(c1.color);
+
+//         System.out.println(c2.brand);
+//         System.out.println(c2.brand);
+
+//         c1.drive();
+//         c2.drive();
+//     }
+// }
+
+
+class Rectangle {
+    int length;
+    int width;
+
+    void main () {
+        int area = length * width;
+        System.out.println(area);
     }
 }
 public class oop {
     public static void main(String[] args) {
-        Car c1 = new Car();
+        Rectangle r1 = new Rectangle();
 
-        c1.brand = "BMW";
-        c1.color = "Black";
+        r1.length = 10;
+        r1.width = 5;
 
-        Car c2 = new Car(); 
-
-        c2.brand = "Audi";
-        c2.color = "White";
-
-        System.out.println(c1.brand);
-        System.out.println(c1.color);
-
-        System.out.println(c2.brand);
-        System.out.println(c2.brand);
-
-        c1.drive();
-        c2.drive();
+        r1.main();
     }
 }
