cbad66f8322044140f27a2b5a8ebc7ae6e2510b6
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-29 15:22:36 +0530

    type

--- java practice/Exception.java
+++ java practice/Exception.java
@@ -18,14 +18,76 @@
 
 //  UnChacked Exception
 
+// class Exception {
+//     public static void main(String[] args) {
+
+//         int a = 10;
+//         int b = 0;
+
+//         int result = a / b;
+
+//         System.out.println(result);
+//     }
+// }
+
+
+// Try
+
+// class Exception {
+//     public static void main(String[] args) {
+//         try {
+//             int result = 10/0;
+//             System.out.println(result);
+//         }
+//         catch (ArithmeticException e){
+//             System.out.println("Cannot divide by zero");
+//         }
+//     }
+// }
+
+
+// catch (ArithmeticException e)
+
+// class Exception {
+//     public static void main(String[] args) {
+//         try {
+//             int result = 10/0;
+//             System.out.println(result);
+//         }
+//         catch (ArithmeticException e){
+//             System.out.println(e);
+//         }
+//     }
+// }
+
+// e.getMessage()
+
+
+// class Exception {
+//     public static void main(String[] args) {
+//         try {
+//             int result = 10/0;
+//             System.out.println(result);
+//         }
+//         catch (ArithmeticException e){
+//             System.out.println(e.getMessage());
+//         }
+//     }
+// }
+
+
+// e.printStackTrace()
+
 class Exception {
     public static void main(String[] args) {
-
-        int a = 10;
-        int b = 0;
-
-        int result = a / b;
-
-        System.out.println(result);
+        try {
+            int result = 10/0;
+            System.out.println(result);
+        }
+        catch (ArithmeticException e){
+            e.printStackTrace();
+        }
     }
 }
+
+
