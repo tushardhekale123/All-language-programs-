28e116593b2c261e6fefc82d70f30ef95ed3e0db
tushardhekale123 <tushardhekale884@gmail.com>
2026-08-29 15:41:20 +0530

    Finally

--- java practice/Exception.java
+++ java practice/Exception.java
@@ -78,16 +78,32 @@
 
 // e.printStackTrace()
 
-class Exception {
-    public static void main(String[] args) {
-        try {
-            int result = 10/0;
-            System.out.println(result);
-        }
-        catch (ArithmeticException e){
-            e.printStackTrace();
-        }
-    }
-}
+// class Exception {
+//     public static void main(String[] args) {
+//         try {
+//             int result = 10/0;
+//             System.out.println(result);
+//         }
+//         catch (ArithmeticException e){
+//             e.printStackTrace();
+//         }
+//     }
+// }
 
 
+//  finally
+
+// class Exception {
+//     public static void main(String[] args) {
+//         try {
+//             int result = 10/0;
+//             System.out.println(result);
+//         }
+//         catch (ArithmeticException e) {
+//             System.out.println("Cannot divide by zero");
+//         }
+//         finally {
+//             System.out.println("Finally execute");
+//         }
+//     }
+// }
